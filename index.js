53d9acf61982b39d80757569a2a68c855dc302f7
Anthony Gorski <anthony.developpeurweb@gmail.com>
2022-10-06 12:13:20 +0200

    tp done

--- index.js
+++ index.js
@@ -1,103 +1,112 @@
-/** Je récupère mes informations depuis le DOM */
+/** Get information depuis le DOM */
 const FORM = document.querySelector("form");
 const INPUT_TODO = document.querySelector(".insertTodo");
 const LISTS = document.querySelector(".lists");
 
-// Ajout des taches en dur pour les tests, c'est plus facile pour débugguer
-let todoList = {
-  todo1: {
-    todo: "Faire à manger",
-  },
-  todo2: {
-    todo: "Faire la vaisselle",
-  },
-  todo3: {
-    todo: "Faire des vidéos",
-  },
-};
+let todoList = {};
 
-// Je créer 2 fonctions qui permettrons de supprimer ou check
+// J'attend que mon ma page soit en load et je lance ma fonction
+// doc : https://developer.mozilla.org/fr/docs/Web/API/Window/load_event
+window.addEventListener("load", loadData);
+
+// Sur mon form des qu'il y a un submit, je lance la fonction createItem
+FORM.addEventListener("submit", createItem);
+
+/** Mes fonctions **/
 
 // Trash
 function trash() {
-  // console.log(this);
-  // Je récupère l'élément parent de mon bouton trash
+  // Je récupère le parent du parent, et je le remove
   this.parentNode.parentNode.remove();
+  // Je récupère l'id passer au front
+  const key = this.parentNode.parentNode.getAttribute("data-id");
+  // et le delete de mon objet todolist
+  delete todoList[key];
+  // Je save l'info
+  saveData();
 }
 
 // Check
 function check() {
   // Operateur ternaire
-  //console.log(this);
+  // doc : https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Operators/Conditional_Operator
   this.innerHTML = this.innerHTML === "✅" ? "❌" : "✅";
+  // Je récupère ma span et lui injecte mon css
+  const span = this.parentNode.parentNode.children[0];
+  span.classList.toggle("done");
+  // Je récupère la clé, et selon son état, je lui indique je met l'inverse.
+  const key = this.parentNode.parentNode.getAttribute("data-id");
+  todoList[key].checked = !todoList[key].checked;
+  // J'enregistre les infos
+  saveData();
 }
 
-// loadData() permet de chargé les informations une fois le DOM chargé
+// Le localStorage, celui ci permet d'enregistrer les infos dans la memoire de l'ordi
+function saveData() {
+  window.localStorage.setItem("data", JSON.stringify(todoList));
+}
+
+// La fonction loadData, permet d'aller récupèrer les infos du localstorage et de les restituer dans le DOM
 function loadData() {
   console.log("Je suis load !");
-  Object.keys(todoList).map((key) => createHTML(todoList[key].todo));
+  // Si j'ai rien dans mon local storage, tu retournes
+  if (!window.localStorage.getItem("data")) return;
+
+  // Sinon, tu récupères les info et tu les mets dans data
+  const data = JSON.parse(window.localStorage.getItem("data"));
+  todoList = data;
+  // J'utilise le .map() pour restituer chaque taches
+  // doc : https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+  Object.keys(todoList).map((key) => createHTML(todoList[key], key));
 }
 
-// Je créer une fonction pour créer ma liste de to do dans mon HTML
-function createHTML(todo) {
-  /** Si pas de todo, je ne fais rien, donc return **/
-  if (!todo) return;
+// Creation de HTML
+function createHTML(todo, key) {
+  // Si pas de data, return
+  if (!todo.todo) return;
 
   const html = `
-  <span>${todo}</span>
+  <span class="${todo.checked ? "done" : ""}">${todo.todo}</span>
   <span>
     <button name="trash" class="trash"><i class="fas fa-trash-alt"></i></button>
-    <button name="check" class="check">✅</button>
+    <button name="check" class="check">${todo.checked ? "❌" : "✅"}</button>
   </span>
   `;
 
-  // Ici, je créer un élément li
-  // doc: https://developer.mozilla.org/fr/docs/Web/API/Document/createElement
+  // Ici, j'informe que
   const li = document.createElement("li");
-  // Je lui ajoute en HTLM ma variable html
+  // J'ajoute l'id à mon li pour le reconnaitre
+  li.setAttribute("data-id", key);
   li.innerHTML = html;
-  // Ici, j'ajoute à ma liste de to do, mon élément li avec son contenu
-  // doc: https://developer.mozilla.org/fr/docs/Web/API/Node/insertBefore
-  // LISTS.firstChild = le premier enfant de LISTS
+  // doc : https://developer.mozilla.org/fr/docs/Web/API/Node/insertBefore
   LISTS.insertBefore(li, LISTS.firstChild);
 
-  // Ici, je récupère l'info de button et en fonction je trash ou je check
-  // console.log(li.children[1].children[0]);
+  // Permet de voir le contenu pour comprendre de quoi il est composé
+  // console.log(li.children[1].children.trash);
   li.children[1].children.trash.onclick = trash;
   li.children[1].children.check.onclick = check;
 }
 
+// Creer un item
 function createItem(event) {
-  // je stoppe le comportement par défaut du formulaire
   event.preventDefault();
 
-  // Je dois identifier de maninière unique ma task
-  // Alors je lui ajoute un Date.now()
-  const timestamp = Date.now();
-  // Je créer un objet avec ma task
-  // La notation [] permet de créer une clé dynamiquement
-  todoList[timestamp] = {
-    // je créer le même objet que dans l'exemple avec
-    // en plus savoir si c'est coché ou pas
-    todo: INPUT_TODO.value,
-    checked: false,
-  };
+  // Si c'est pas vide
+  if (INPUT_TODO.value) {
+    // Permet de donner un ID unique
+    const timestamp = Date.now();
+    // Et je lui donne les info
+    todoList[timestamp] = {
+      todo: INPUT_TODO.value,
+      checked: false,
+    };
 
-  createHTML(todoList[timestamp].todo);
+    // Je fait appel a la fonction createHTML
+    createHTML(todoList[timestamp], timestamp);
 
-  // console.log(todoList);
-  /**
-   * output:
-   * 1665049634136 : {todo: 'hello wilders!', checked: false}
-   * todo1 : {todo: 'Faire à manger'}
-   * todo2 : {todo: 'Faire la vaisselle'}
-   * todo3 : {todo: 'Faire des vidéos'}
-   *
-   */
-  // Je vide mon input
+    // Je save le changement
+    saveData();
+  }
+  // Je reset l'input
   INPUT_TODO.value = "";
 }
-
-// et je l'utilise ici
-window.addEventListener("load", loadData);
-FORM.addEventListener("submit", createItem);
